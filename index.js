2cfed9fdf8151d5e2af925f786a194975a87ea86
Sean <craig.sean92@gmail.com>
2021-12-10 15:10:33 -0700

    commit tree 4e27b71e5341450dd2a715cfe04013944f0f3912

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Your code here
+function createEmployeeRecord(array) {
+                let obj = {
+                firstName: array[0],    
+                familyName: array[1],
+                title: array[2],
+                payPerHour: array[3],
+                timeInEvents: [],
+                timeOutEvents: [],
+                }
+            return obj 
+          }
+ 
+function createEmployeeRecords(array) {
+
+    let newArr = []
+    for(let i = 0; i < array.length; i++){
+    newArr.push(createEmployeeRecord(array[i]))   
+    
+}
+    return newArr
+}
+
+function createTimeInEvent(employee ,datestamp) {
+  
+  const string = datestamp
+ 
+  const timeInEvent = {
+    type: "TimeIn",
+    hour: String(string).substring(11,15),
+    date: String(string).substring(0,10),
+  }
+  employee.timeInEvents.push(timeInEvent)
+  console.log(employee)
+}
+
+
+
+// let twoRows = [
+//     ["moe", "sizlak", "barkeep", 2],
+//     ["bartholomew", "simpson", "scamp", 3]
+//   ]
+
+//   let dataEmployees = [
+//     ["Thor", "Odinsson", "Electrical Engineer", 45],
+//     ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+//     ["Natalia", "Romanov", "CEO", 150],
+//     ["Darcey", "Lewis", "Intern", 15],
+//     ["Jarvis", "Stark", "CIO", 125],
+//     ["Anthony", "Stark", "Angel Investor", 300],
+//     ["Byron", "Poodle", "Mascot", 3],
+//     ["Julius", "Caesar", "General", 27],
+//     ["Rafiki", "", "Aide", 10],
+//     ["Simba", "", "King", 100]
+//   ]
